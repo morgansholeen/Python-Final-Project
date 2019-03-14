ab8362c4a7c686eca930332267faf6e110be4f7e
Swetha Berana <45606459+swethaberana@users.noreply.github.com>
2019-03-14 02:01:49 -0500

    added practice mode, language selection functionality and text to speech!
    text to speech a little glitchy and fast! 
    still gotta tag the translated words and say them with foreign accent, will fix when we fix speech speed issue.

--- noun replacement woo.py
+++ noun replacement woo.py
@@ -1,26 +1,52 @@
 import nltk
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 from googletrans import Translator
+import pyttsx3
+engine = pyttsx3.init()
 translator = Translator()
+from nltk.tokenize import word_tokenize
+
+dict1 = []
+result = ''
+destlang= 'es'
 
 while(1):
-    from nltk.tokenize import word_tokenize
-    sentence = input("Welcome to SpInPy! Enter '*' to exit.\nPlease type a sentence in English: ")
-    if sentence == '*':
-        break
-    words = word_tokenize(sentence)
-    if words[-1] == '.':
+   mode = input("Welcome to SpInPy! Enter 1 for speech mode, 2 for dictionary mode or 3 for language settings: ") 
+   imode = int(mode)
+   if (imode== 1):
+        sentence = input("Please type a sentence in English: ")
+        words = word_tokenize(sentence)
         del words[-1]
-    tagged_words = nltk.pos_tag(words)
-    print(tagged_words)
+        tagged_words = nltk.pos_tag(words)
+        #print(tagged_words)
+        for word in tagged_words:
+            if word[1] == 'NN' or word[1] == 'NNS':
+                es_word = (translator.translate(word[0], dest=destlang).text)
+                words[tagged_words.index(word)] = es_word
+                dict1.extend((es_word, word[0]))
+        s = ' '
+        cs_sentence = s.join(words) 
+        result = cs_sentence + '.'
 
-    for word in tagged_words:
-        if word[1] == 'NN' or word[1] == 'NNS':
-            es_word = (translator.translate(word[0], dest='es').text)
-            i = tagged_words.index(word)
-            words[i] = es_word
+   if (imode== 2):
+        result=dict1
 
-    s = ' '
-    cs_sentence = s.join(words) 
-    cs_sentence = cs_sentence + '.'
-    print(cs_sentence)
+   if (imode== 3):
+       alang = ''
+       lang = input("Enter f for French, g for German or s for Spanish (default): ")
+       ilang = str(lang)
+       if(ilang == 'f'):
+           alang = 'fr'
+       if(ilang == 'g'):
+           alang = 'ge'
+       if(ilang == 's'):
+           alang = 'es'
+       destlang=alang
+           
+          
+
+   if imode== 1 or imode==2:        
+       print(result)
+       engine.say(result)
+       engine.runAndWait()
