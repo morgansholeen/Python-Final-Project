869d89ec26e82411c88b3788e013c40e9a0db319
lindiatjuatja <47533416+lindiatjuatja@users.noreply.github.com>
2019-03-14 20:50:30 -0500

    Update noun replacement woo.py
    Better user interface functionality. Allows for exiting to main menu, and from main menu to quit.

--- noun replacement woo.py
+++ noun replacement woo.py
@@ -1,3 +1,4 @@
+#modules to be used
 import nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -7,19 +8,26 @@ engine = pyttsx3.init()
 translator = Translator()
 from nltk.tokenize import word_tokenize
 
+#global variables
 dict1 = []
 result = ''
 destlang= 'es'
 
-while(1):
-   mode = input("Welcome to SpInPy! Enter 1 for speech mode, 2 for dictionary mode or 3 for language settings: ") 
-   imode = int(mode)
-   if (imode== 1):
-        sentence = input("Please type a sentence in English: ")
+#tts function for mode (1)
+#called based on user input
+def speak(result):
+    engine.say(result)
+    engine.runAndWait()
+
+#codeswitch function for mode (1)
+def codeswitch():
+    while(1): 
+        sentence = input("Type a sentence in English: ")
+        if sentence == '*':
+            break
         words = word_tokenize(sentence)
         del words[-1]
         tagged_words = nltk.pos_tag(words)
-        #print(tagged_words)
         for word in tagged_words:
             if word[1] == 'NN' or word[1] == 'NNS':
                 es_word = (translator.translate(word[0], dest=destlang).text)
@@ -28,28 +36,50 @@ while(1):
         s = ' '
         cs_sentence = s.join(words) 
         result = cs_sentence + '.'
+        listen = input(f'{result}\nEnter (@) to listen, (n) to enter a new sentence: ')
+        if (listen == 'n'):
+            codeswitch()
+        elif (listen == '@'):
+            speak(result)
+            codeswitch()
+        return; 
 
-   if (imode== 2):
-        result=dict1
+#outer loop - main menu and exit
+while(1):
+   mode = input("Welcome to SpInPy!\n Main menu-- (1) code switch    (2) dictionary   (3) change language   (*) exit\n Mode select: ") 
+   imode = mode
+   
+   if (imode == '*'):
+       print('¡Adios¡ Goodbye!')
+       break
+   
+   if (imode == '1'):
+     print('\nCode switch mode. Enter (*) to go back to the main menu.')
+     codeswitch()
+     
+#   if (imode== '2'):
+#        result=dict1
 
-   if (imode== 3):
+   if (imode== '3'):
+       flag = 1
        alang = ''
-       lang = input("Enter f for French, g for German or s for Spanish (default): ")
-       ilang = str(lang)
-       if(ilang == 'f'):
-           alang = 'fr'
-       if(ilang == 'g'):
-           alang = 'de'
-       if(ilang == 's'):
-           alang = 'es'
-       destlang=alang
+       while(flag):
+           lang = input("To return to the main menu, enter (*)\nEnter (f) for French, (g) for German or (s) for Spanish (default): ")
+           if (lang == '*'):
+               flag = 0;
+           elif (lang == 'f'):
+               alang = 'fr'
+               flag = 0
+           elif (lang == 'g'):
+               alang = 'de'
+               flag = 0
+           elif(lang == 's'):
+               alang = 'es'   
+               flag = 0
+           else: 
+               print("\nNot a valid input. ")
+       destlang = alang
            
-          
-
-   if imode== 1 or imode==2:        
-       print(result)
-       engine.say(result)
-       engine.runAndWait()
-
-   if imode==2:       
-       dict1.clear()
+#   if imode==2:       
+#   dict1.clear()
+        
